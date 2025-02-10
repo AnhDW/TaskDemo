aa928982731a657fcc526321bd56a63da47ae4aa
vu anh <vungocanh723@gmail.com>
2025-02-10 17:01:18 +0700

    Add project files.

---
+++ TaskDemo/Controllers/SampleDataController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using TaskDemo.Models;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskDemo.Controllers {
+
+    [Route("api/[controller]")]
+    public class SampleDataController : Controller {
+
+        [HttpGet]
+        public object Get(DataSourceLoadOptions loadOptions) {
+            return DataSourceLoader.Load(SampleData.Orders, loadOptions);
+        }
+
+        [HttpGet]
+        public object GetProduct(DataSourceLoadOptions loadOptions)
+        {
+            return DataSourceLoader.Load(SampleData.Products, loadOptions);
+        }
+
+    }
+}
---
+++ TaskDemo/Models/SampleProduct.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskDemo.Models
+{
+    public class SampleProduct
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
